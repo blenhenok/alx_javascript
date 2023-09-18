485c6c90ec08c1a51d63de990d9e314370346bbf
blen <blenhenok93@gmail.com>
2023-09-18 17:25:07 +0300

    Star wars movie title

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
 const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movie_id}`; {
+request('https://swapi-api.alx-tools.com/api/films/' + id + '/', function (error, response, body) {
   if (error == null) {
     const json = JSON.parse(body);
     console.log(json.title);
   }
-};
+});
