7c7e05b3063b9da3de908881984ae858951ef6d9
blen <blenhenok93@gmail.com>
2023-09-18 17:20:52 +0300

    Star wars Wedge Antilles

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,15 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
+let nFilms = 0;
 
-function count_movies_with_wedge_antilles(api_url) {
-    const character_id = 18;
-    request(api_url, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            const films_data = JSON.parse(body);
-            const movies_with_wedge_antilles = films_data.results.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${character_id}/`));
-            console.log(`Number of movies with Wedge Antilles: ${movies_with_wedge_antilles.length}`);
-        } else {
-            console.log("Error: Failed to fetch movie data");
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    const resp = JSON.parse(body);
+    const results = resp.results;
+    for (let i = 0; i < results.length; i++) {
+      const characters = results[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j].search('18') > 0) {
+          nFilms++;
         }
-    });
-}
+      }
+    }
+  }
+  console.log(nFilms);
+});
